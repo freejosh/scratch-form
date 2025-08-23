168138cee05e655d65c1c08e06d1872dd00ee5cc
Josh Freeman <freejosh@gmail.com>
2025-08-23 18:02:56 -0300

    Initial implementation & demo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,50 @@
-function ScratchForm(element, onChange) {
-  return {};
+const PREFIX = '_sf_';
+
+function ScratchForm(formElement, options = {}) {
+  const {
+    onChange,
+  } = options;
+
+  const data = {};
+  const handler = {
+    set(obj, rawName, rawValue) {
+      let name = rawName;
+      let value = rawValue;
+      let node;
+
+      if (rawName.startsWith(PREFIX)) {
+        // change came from form - parse data format
+        name = rawName.slice(PREFIX.length);
+        ({ value, node } = rawValue);
+      } else {
+        // change came from object - set value on form
+        node = formElement.querySelector(`[name="${name}"]`);
+        if (node) {
+          node.value = rawValue;
+        }
+      }
+
+      obj[name] = value;
+      onChange(name, value, obj, node);
+
+      return true;
+    },
+  };
+
+  const proxy = new Proxy(data, handler);
+
+  // initialize with current form values
+  Array.from(formElement.querySelectorAll('[name]')).forEach((el) => {
+    proxy[el.name] = el.value;
+  });
+
+  // bind handler to form
+  formElement.addEventListener('change', (e) => {
+    const { name, value } = e.target;
+    proxy[`${PREFIX}${name}`] = { value, node: e.target };
+  });
+
+  return proxy;
 }
 
 window.ScratchForm = ScratchForm;
