24dcc475d16765f2b3f220a6d278ffda144f0916
Josh Freeman <freejosh@gmail.com>
2025-09-01 15:56:57 -0300

    Move demo files for GH page

---
+++ demo/main.js
@@ -0,0 +1,44 @@
+let data;
+
+function random(max) {
+  return Math.floor(Math.random() * max);
+}
+
+let timeout;
+function startInterval() {
+  timeout = setTimeout(() => {
+    data.number = random(data.range);
+    startInterval();
+  }, data.interval);
+}
+function stopInterval() {
+  clearTimeout(timeout);
+}
+
+const textarea = document.getElementById('data');
+data = new ScratchForm(document.getElementById('form'), {
+  onChange(name, value, node, obj) {
+    if (name === 'toggle') {
+      if (value) {
+        startInterval();
+      } else {
+        stopInterval();
+      }
+    }
+
+    textarea.value = JSON.stringify(obj, null, 2);
+  },
+});
+
+document.getElementById('add-color').addEventListener('click', (e) => {
+  const newEl = document.querySelector('[name="colors[]"]').cloneNode();
+  newEl.value = `#${random(255).toString(16).padStart(2, '0')}${random(255).toString(16).padStart(2, '0')}${random(255).toString(16).padStart(2, '0')}`;
+  e.target.parentNode.insertBefore(newEl, e.target);
+});
+
+document.getElementById('remove-color').addEventListener('click', () => {
+  const els = document.querySelectorAll('[name="colors[]"]');
+  if (els.length > 1) {
+    els[els.length - 1].remove();
+  }
+});
