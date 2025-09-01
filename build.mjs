f6adf6fb1b95c6ff722d0ee461fd599ba145595e
Josh Freeman <freejosh@gmail.com>
2025-09-01 12:00:10 -0300

    Bundle with UMD, prep for npm package

--- build.mjs
+++ build.mjs
@@ -1,28 +1,32 @@
 import * as esbuild from 'esbuild';
 import { clean } from 'esbuild-plugin-clean';
 import browserslistToEsbuild from 'browserslist-to-esbuild';
+import { umdWrapper } from 'esbuild-plugin-umd-wrapper';
 
 const isProd = process.env.NODE_ENV === 'production';
 
-const BUILD_DIR = './demo';
-const MAIN_FILE = 'main.js';
+const BUILD_DIR = './dist';
+const LIB_NAME = 'ScratchForm';
 
 (async () => {
   const options = {
-    entryPoints: [`./src/${MAIN_FILE}`],
+    entryPoints: [`./src/${LIB_NAME}.js`],
     bundle: true,
     sourcemap: !isProd,
-    outdir: BUILD_DIR,
+    outfile: `${BUILD_DIR}/${LIB_NAME}.js`,
     minify: isProd,
     platform: 'browser',
-    format: 'iife',
-    globalName: 'ScratchForm',
+    format: 'umd',
+    globalName: LIB_NAME,
     target: browserslistToEsbuild('defaults'),
     plugins: [
       clean({
         patterns: [
-          `${BUILD_DIR}/main.js*`,
+          `${BUILD_DIR}/${LIB_NAME}*`,
         ],
+      }),
+      umdWrapper({
+        libraryName: LIB_NAME,
       }),
     ],
   };
