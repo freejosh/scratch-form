ade7c4ca850793e2798cd36b89d1dfb4581b148b
Josh Freeman <freejosh@gmail.com>
2025-09-09 12:54:45 -0300

    Switch to eslint airbnb-base to remove react config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: [
-    'airbnb',
+    'airbnb-base',
     'plugin:@typescript-eslint/recommended',
   ],
   env: {
